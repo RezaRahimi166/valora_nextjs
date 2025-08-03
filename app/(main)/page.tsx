df2e3d01235bd3dca425e538d20d465ce83568ba
RezaRahimi166 <rezaa.salami99@gmail.com>
2025-08-03 23:13:25 +0330

    Add dropdown menu component and theme toggle functionality; integrate next-themes for theme management in layout.

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,8 +1,7 @@
-import { Button } from "@/components/ui/button";
 import React from "react";
 
 const HomePage = () => {
-  return <Button>HomePage</Button>;
+  return <div>HomePage</div>;
 };
 
 export default HomePage;
