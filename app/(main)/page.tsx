dff9f6fd271f58266d43349250c97a237f9e9a38
RezaRahimi166 <rezaa.salami99@gmail.com>
2025-08-04 00:06:16 +0330

    Add ProductList component to HomePage; integrate sample data for displaying product listings with a title and limit option.

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,17 @@
+import ProductList from "@/common/product/product-list";
+import sampleData from "@/db/sample-data";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default HomePage;
