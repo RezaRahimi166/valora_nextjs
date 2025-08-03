0e3702c5380c2ec882fbe78e5f9f4c359d5d30ed
RezaRahimi166 <rezaa.salami99@gmail.com>
2025-08-03 22:28:38 +0330

    Implement layout structure and metadata configuration; add MainLayout and HomePage components, and define application constants.

---
+++ lib/constants/index.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Velora";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Modern E-commerce Platform";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
