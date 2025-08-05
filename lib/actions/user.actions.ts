ab7ff2c689eea7062fa7919a09b73a6e49c82f40
RezaRahimi166 <rezaa.salami99@gmail.com>
2025-08-05 19:53:40 +0330

    feat(auth): add sign-in form validation schema and user authentication actions

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,36 @@
+"use server";
+import { signIn, signOut } from "@/auth";
+import { signInFormSchema } from "../validators";
+import { isRedirectError } from "next/dist/client/components/redirect-error";
+
+// Singin user with credentials
+export async function signInWithCredentials(
+  prevState: unknown,
+  formData: FormData
+) {
+  try {
+    const user = signInFormSchema.parse({
+      email: formData.get("email"),
+      password: formData.get("password"),
+    });
+
+    await signIn("credentials", user);
+    return {
+      success: true,
+      message: "Signed in successfuly",
+    };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+    return {
+      success: false,
+      message: "Invalid email or password",
+    };
+  }
+}
+
+// Sign user out
+export async function signOutUser() {
+  await signOut();
+}
