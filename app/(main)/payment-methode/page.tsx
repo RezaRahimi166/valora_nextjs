5632fc6830e28f795a35dcff2ce2aef8fff818d7
RezaRahimi166 <rezaa.salami99@gmail.com>
2025-08-07 03:19:43 +0330

    feat(payment-methode): crete validatios and so on

--- app/(main)/payment-methode/page.tsx
+++ app/(main)/payment-methode/page.tsx
@@ -1,11 +1,26 @@
+import { auth } from "@/auth";
 import CheckoutSteps from "@/common/checkout/checkout-steps";
+import PaymentMethodForm from "@/components/(main)/paymentMethod/payment-method-form";
+import { getUserById } from "@/lib/actions/user.actions";
+import { Metadata } from "next";
 import React from "react";
 
-const PaymentMethode = () => {
+export const metadata: Metadata = {
+  title: "Select Payment Method",
+};
+
+const PaymentMethode = async () => {
+  const seesion = await auth();
+  const userId = seesion?.user?.id;
+
+  if (!userId) throw new Error("User not found");
+
+  const user = await getUserById(userId);
+
   return (
     <>
       <CheckoutSteps current={2} />
-      Payment
+      <PaymentMethodForm preferredPaymentMethod={user.paymentMethod} />
     </>
   );
 };
