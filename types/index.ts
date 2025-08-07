c957d778ad3183ce3d95587d0afadab9da6867df
RezaRahimi166 <rezaa.salami99@gmail.com>
2025-08-07 03:51:56 +0330

    feat(model): create order and orderid model

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,8 @@
 import {
   cartItemSchema,
   insertCartSchema,
+  insertOrderItemSchema,
+  insertOrderSchema,
   insertProductSchema,
   shippingAddressSchema,
 } from "@/lib/validators";
@@ -18,3 +20,16 @@ export type CartItem = z.infer<typeof cartItemSchema>;
 
 // shipping address
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
+
+// order && orderitem
+export type OrderItem = z.infer<typeof insertOrderItemSchema>;
+export type Order = z.infer<typeof insertOrderSchema> & {
+  id: string;
+  createdAt: Date;
+  isPaid: Boolean;
+  paidAt: Date | null;
+  isDelivered: Boolean;
+  deliveredAt: Date | null;
+  orderitems: OrderItem[];
+  user: { name: string; email: string };
+};
