a8af67226d10dcebcae78260693fd0cb215f63e8
RezaRahimi166 <rezaa.salami99@gmail.com>
2025-08-08 19:48:13 +0330

    feat(config): enable Node.js runtime for middleware in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    nodeMiddleware: true, // 🚀 فعال کردن Node.js runtime برای middleware
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
