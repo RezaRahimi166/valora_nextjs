dc716e6d5be6823f476781b030d7b1863d5115e8
RezaRahimi166 <rezaa.salami99@gmail.com>
2025-08-16 21:50:53 +0330

    feat(home): add carousel

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,11 +1,19 @@
+import ProductCarousel from "@/common/product/product-carousel";
 import ProductList from "@/common/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import {
+  getFeaturedProducts,
+  getLatestProducts,
+} from "@/lib/actions/product.actions";
 import React from "react";
 
 const HomePage = async () => {
   const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
   return (
     <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
       <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
     </>
   );
